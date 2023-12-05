aad0b23784782b8a15a94784bd17ce1c231c2b57
Vandarks <majewski.miiko@gmail.com>
2023-12-05 10:19:08 +0200

    Some new stuff

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.jenkinstest {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.jenkinstest to javafx.fxml;
     exports com.example.jenkinstest;
 }
